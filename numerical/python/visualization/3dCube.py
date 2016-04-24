9851df1757ee1c680ccde00697214e333b64364e
ryu577 <rohitpandey576@gmail.com>
2016-04-24 00:39:15 -0700

    Updated visualize point cloud file

--- numerical/python/visualization/3dCube.py
+++ numerical/python/visualization/3dCube.py
@@ -19,22 +19,28 @@ for s, e in combinations(np.array(list(product(r,r,r))), 2):
     if np.sum(np.abs(s-e)) == r[1]-r[0]:
         ax.plot3D(*zip(s,e), color="g")
 
-
 #draw the origin
 ax.scatter([0],[0],[0],color="g",s=100)
 
+transform_matrix = np.array([np.array([1,0,0]),np.array([1,1,0]),np.array([1,1,1])])
 pos_samples = 0
 for i in xrange(5000):
     x=np.random.uniform(0,1,1)
     y=np.random.uniform(0,1,1)
     z=np.random.uniform(0,1,1)
-    if x > y and y > z and x > z:
-    #if x+y+z<1:
+    #if x > y and y > z:
+    if x+y+z < 1 and x+y+z>.9:
         pos_samples = pos_samples + 1
-        ax.scatter(x,y,z,color="r", s=10)
+        #ax.scatter(x,y,z,color="r", s=10, alpha=.5)
+        trnsfm = transform_matrix.dot(np.array([x[0],y[0],z[0]]))
+        #ax.scatter(x,y,z,color="y",s=20,alpha=.5)
+        #alph = 1/(x+y+z)
+        ax.scatter(trnsfm[0],trnsfm[1],trnsfm[2],color="y",s=20,alpha=.5)
+    if z-y > 0 and z-y < 0.1 and y-x > 0 and y-z < 0.1:
+        ax.scatter(x,y,z,color="b", s=10)
+    elif z>y and y>x:
+        ax.scatter(x,y,z,color="b", s=10, alpha = .1)
 
 print float(pos_samples)/5000
-
-#draw a vector
 
 plt.show()
