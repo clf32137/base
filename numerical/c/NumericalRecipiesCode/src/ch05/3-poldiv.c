b49c6b3d1d49603bf00a118c363ff956756e2866
ryu577 <rohitpandey576@gmail.com>
2016-04-29 19:32:16 -0700

    Added debugging functions

--- numerical/c/NumericalRecipiesCode/src/ch05/3-poldiv.c
+++ numerical/c/NumericalRecipiesCode/src/ch05/3-poldiv.c
@@ -18,6 +18,33 @@ void poldiv(
 
 	for (j = 0; j <= n; j++)
 	{
+		r[j] = u[j];//Remainder equals numerator
+		q[j] = 0.0;
+	}
+	for (k = n - nv; k >= 0; k--)
+	{
+		q[k] = r[nv + k] / v[nv];
+		for (j = nv + k - 1; j >= k; j--)
+			r[j] -= q[k] * v[j - k];
+	}
+	for (j = nv; j <= n; j++)
+		r[j] = 0.0;
+}
+
+
+void poldiv_debug(
+	float u[], //Numerator polynomial
+	int n, //The degree of the numerator
+	float v[], //Denominator polynomial
+	int nv, //Degree of denominator
+	float q[], //OUTPUT Quotiend polynomial
+	float r[]  //OUTPUT Remainder polynomial
+	)
+{
+	int k, j;
+
+	for (j = 0; j <= n; j++)
+	{
 		r[j] = u[j];
 		q[j] = 0.0;
 	}
@@ -27,6 +54,6 @@ void poldiv(
 		for (j = nv + k - 1; j >= k; j--)
 			r[j] -= q[k] * v[j - k];
 	}
-	for (j = nv; j <= n; j++) 
+	for (j = nv; j <= n; j++)
 		r[j] = 0.0;
 }
--- numerical/c/NumericalRecipiesCode/src/ch05/ch5funcs.h
+++ numerical/c/NumericalRecipiesCode/src/ch05/ch5funcs.h
@@ -1,6 +1,6 @@
 
 void ddpoly(float c[], int nc, float x, float pd[], int nd);
+void ddpoly_debug(float c[], int nc, float x, float pd[], int nd);
 void poldiv(float u[], int n, float v[], int nv, float q[], float r[]);
-
-
+void poldiv_debug(float u[],int n,float v[],int nv,float q[],float r[]);
 
