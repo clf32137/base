a0a9ea0603c8c41150c6e27e07ee33715872df67
ryu577 <rohitpandey576@gmail.com>
2016-05-14 15:13:34 -0700

    Fixed chebyshev function parameter order

--- numerical/c/NumericalRecipiesCode/src/ch05/ch5funcs.h
+++ numerical/c/NumericalRecipiesCode/src/ch05/ch5funcs.h
@@ -6,6 +6,6 @@ void poldiv_debug(float u[],int n,float v[],int nv,float q[],float r[]);
 float dfridr(float (*func)(float), float x, float h, float *err);
 float simplenumerical(float (*func)(float), float x, float h);
 void chebft(float a, float b, float c[], int n, float (*func)(float));
-float chebev(float a, float b, float c[], int m, float x);
-float chebev_debug(float a, float b, float c[], int m, float x);
+float chebev(float a, float b, float c[], float x, int m);
+float chebev_debug(float a, float b, float c[],  float x, int m);
 
