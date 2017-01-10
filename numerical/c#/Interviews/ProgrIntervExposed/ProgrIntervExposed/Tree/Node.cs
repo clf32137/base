326324599e47e94326873b4e673b1d3cd592537f
Rohit Pandey <rohitpandey576@gmail.com>
2017-01-10 00:09:29 -0800

    Added nodes and trees

---
+++ numerical/c#/Interviews/ProgrIntervExposed/ProgrIntervExposed/Tree/Node.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrIntervExposed.Tree
+{
+    public class Node
+    {
+        // Properties..
+        public Node Left;
+        public Node Right;
+        public int Value;
+
+        // Constructor
+        public Node(Node left, Node right, int value)
+        {
+            this.Left = left;
+            this.Right = right;
+            this.Value = value;
+        }
+    }
+}
